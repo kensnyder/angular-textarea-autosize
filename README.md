2fc6e37ca1008d52e4b4c54f2b497648ca0d68a3
Ken Snyder <kendsnyder@gmail.com>
2015-05-16 22:11:38 -0600

    first release.

--- README.md
+++ README.md
@@ -1,32 +1,24 @@
-jQuery Autosize
+Angular Textarea Autosize
 =
 
-v1.1.0 February 2015
+v1.1.1 May 2015
 
-Created because other libraries didn't calculate very well for me.
+Created because other libraries use ghost elements which didn't calculate very well for me.
 
 This plugin uses the scrollHeight of the textarea to determine the needed height instead of using a ghost element.
 
 Usage
-```javascript
-$('textarea').autosize({
-	onresize: function() {
-		// optional; fired when size changes
-	},
-	// optional minimum sizing options:
-	// defaults to the line height + effective padding
-	minRows: 3,   // minimum height in number of rows
-	minHeight: 30 // minimum height in pixels
-});
+```html 
+<!-- min rows of 1 -->
+<textarea ng-model="note" autosize></textarea>
+<!-- min rows of 2 -->
+<textarea ng-model="note" autosize rows="2"></textarea>
+<!-- min rows of 3, callback when size changes -->
+<textarea ng-model="note" autosize="{minRows:3, onresize:myHandler}"></textarea>
 ```
 
 Limitations:
-1. If the textarea changes width, box model or line height, you'll have to call `.autosize()` again (e.g. if a textarea has a percent-width based on the window and the window is resized)
-2. If you have placeholder text that wraps to a new line, you'll need to manually set minRows to 2
-
-Note:
-1. Calling `.autosize()` removes all handlers, reinitializes and performs initial resizing; multiple calls to `.autosize()` is ok.
-2. You can extend functionality by editing or extending the $.AutosizedTextarea class this plugin creates. See the source code.
+1. If you have placeholder text that wraps to a new line, you'll need to manually set the rows attribute to fit the placeholder
 
 License
 ==
@@ -34,7 +26,6 @@ MIT License
 
 Credits
 ==
-
 Based on the following code and documentation:
 https://github.com/jackmoore/autosize
 https://github.com/javierjulio/textarea-autosize
--- autosizeDirective.js
+++ autosizeDirective.js
@@ -1,5 +1,5 @@
 /*!
- * jQuery autosize v1.1.1
+ * angular-textarea-autosize v1.1.1
  * (c) 2015 Ken Snyder
  * MIT License
  * 
@@ -9,20 +9,63 @@
  * https://github.com/AndrewDryga/jQuery.Textarea.Autoresize
  * https://developer.mozilla.org/en-US/docs/Web/API/Element.scrollHeight
  */
-(function($, window) {
+(function(angular) {
 	
-	// we need window later to prevent page jumping
-	var $window = $(window);
-	
-	$.AutosizedTextarea = function() {
+	angular.module('textareaAutosize', [])
+	.value('constructor', AutosizedTextarea)
+	.directive('autosize', ['$timeout','$window','constructor',
+  function autosizeDirective($timeout,  $window,  constructor) {
+		/*
+		Usage:
+		<!-- min rows of 1 -->
+		<textarea ng-model="note" autosize></textarea>
+		<!-- min rows of 2 -->
+		<textarea ng-model="note" autosize rows="2"></textarea>
+		<!-- min rows of 3, callback when size changes -->
+		<textarea ng-model="note" autosize="{minRows:3, onresize:myHandler}"></textarea>
+		*/
+		return {
+			restrict: 'A',
+			scope: {
+				options: '=?autosize'
+			},
+			require: '?ngModel',
+			link: function autosizeDirectiveLink($scope, $textarea, attrs, ngModel) {
+				$scope.options = $scope.options || {};
+				var sizer = new constructor({
+					$scope: $scope,
+					$textarea: $textarea,
+					attrs: attrs,
+					$window: $window,
+					ngModel: ngModel
+				});
+				$timeout(function() {
+					sizer.adjust();
+				});
+			}
+		};
+	}]);
+
+	function debounce(ms, fn) {
+		var handle;
+		return function() {
+			clearTimeout(handle);
+			handle = setTimeout(fn, ms);
+		};
+	}	
+
+	function AutosizedTextarea() {
 		this.initialize.apply(this, [].slice.call(arguments));
-	};
+	}
 	
-	$.AutosizedTextarea.prototype = {
-		initialize: function(textarea, options) {
-			this.$textarea = $(textarea);
+	AutosizedTextarea.prototype = {
+		initialize: function(vars) {
+			this.$scope = vars.$scope;
+			this.$window = vars.$window;
+			this.$textarea = vars.$textarea;
+			this.ngModel = vars.ngModel;
+			this.attrs = vars.attrs;
 			this.textarea = this.$textarea.get(0);
-			this.options = options || {};
 			this.setup();
 			this.observe();
 			this.adjust();
@@ -54,57 +97,44 @@
 				this.$textarea.css('-moz-box-sizing') == 'border-box'
 			);
 			this.verticalPadding = (this.isBorderBox ? 0 : Math.ceil(this.calculatedPadding));
-			if (this.options.minHeight > 0) {
-				this.minHeight = this.options.minHeight;
-			}
-			else if (this.options.minRows > 0) {
-				this.minHeight = this.options.minRows * this.lineHeight + (this.isBorderBox ? this.calculatedPadding : 0);
-			}
-			else {
-				this.minHeight = this.verticalPadding + this.lineHeight;
-			}
-			// round up any fractional parts
-			this.minHeight = Math.ceil(this.minHeight);
+			this.minHeight = Math.ceil((this.$scope.options.minRows || parseFloat(this.attrs.rows) || 1) * this.lineHeight + (this.isBorderBox ? this.calculatedPadding : 0));
 		},
 		observe: function() {
-			var events = 'input.autosize';
+			var events = 'input';
 			if ('onpropertychange' in this.textarea) {
 				// Detects IE9. IE9 does not fire oninput for deletions,
 				// so binding to onkeyup to catch most of those occasions.
-				events += ' keyup.autosize';
+				events += ' keyup';
 			}
-			this.$textarea
-				// stop observing if .autosize() has been called before
-				.off(events)
-				// observe with this.adjust() (bind is supported by IE9+)
-				.on(events, this.adjust.bind(this))
-			;
+			var self = this;
+			// listen for both 
+			var adjust = debounce(0, function() {
+				self.adjust();
+			});
+			this.$textarea.on(events, adjust);
+			if (this.ngModel) {
+				this.ngModel.$viewChangeListeners.push(adjust);
+			}
 		},
 		adjust: function() {
-			if (this.options.onresize) {
-				var heightBefore = this.textarea.style.height;
+			if (this.$scope.options.onresize) {
+				var oldHeight = this.textarea.style.height;
 			}
-			var currentWindowScroll = $window.scrollTop();
+			var currentWindowScroll = this.$window.scrollTop;
 			// ensure that content can't fit so scrollHeight will be correct
 			this.textarea.style.height = '0';
 			// set height that is just tall enough
-			// noe that scrollHeight is always an integer
+			// note that scrollHeight is always an integer
 			var newHeight = Math.max(this.minHeight, this.textarea.scrollHeight - this.verticalPadding);
 			this.textarea.style.height = newHeight + 'px';
 			// put the window scroll position back
 			// since setting height to 0 may cause window scroll to change
-			$window.scrollTop(currentWindowScroll);
+			this.$window.scrollTop = currentWindowScroll;
 			// trigger resize callback if height has changed
-			if (this.options.onresize && heightBefore != newHeight) {
-				this.options.onresize.call( this, parseFloat(heightBefore), newHeight );
+			if (this.$scope.options.onresize && oldHeight != newHeight) {
+				this.$scope.options.onresize.call( this, parseFloat(oldHeight), newHeight );
 			}
 		}
 	};
-	
-    $.fn.autosize = function(options) {
-        return this.each(function() {
-            new $.AutosizedTextarea(this, options);
-        });
-    };
 
-})(jQuery, window);
+})(angular);
