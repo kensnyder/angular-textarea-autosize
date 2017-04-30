fc9dd4544eb687ad366da84365e4f189e5f2c7f8
Ken Snyder <kendsnyder@gmail.com>
2017-04-30 14:10:38 -0600

    Simplify autosize further, require ngModel, drop IE 9 support.

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 Angular Textarea Autosize
 =
 
-v1.2.1 April 2017
+v2.0.0 April 2017
 
 Created because other libraries use ghost elements which didn't calculate very well for me, especially when using web fonts.
 
@@ -13,8 +13,6 @@ Usage
 <textarea ng-model="note" autosize></textarea>
 <!-- min rows of 2 -->
 <textarea ng-model="note" autosize rows="2"></textarea>
-<!-- min rows of 1, callback when size changes -->
-<textarea ng-model="note" autosize="myOnResizeHandler"></textarea>
 ```
 
 Edge cases:
@@ -23,8 +21,14 @@ Edge cases:
 2. The css property "resize" is forced to "none" so the textbox will have no resize handle
 3. If the textarea has a CSS transition that applies to the height, calculations will be off and size changes may appear janky
 4. If the textarea has a percent width and the window changes size, the autosizer will not be triggered until the next keystroke
-5. The autosizer will not respond to changes in the rows attribute or the computed css values for border, padding, box-sizing or line-height. A hack is to call .reinitAutosize() on the raw DOM element if you know any of those things happen.
-6. Only supports IE9+.
+5. The autosizer will not respond to changes in the rows attribute
+6. If the autosizer uses the transitioned to catch the changes in the computed css values for border, padding, box-sizing and line-height.
+Examples. If `transition: all` and you change the padding, autosizer will respond.
+If `transition: line-height` and you change the line-height, autosizer will respond.
+If `transition: opacity` and you change the border, autosizer will NOT respond.
+If you have no transition defined and you change the padding, autosizer will respond.
+
+A hack is to call .reinitAutosize() on the raw DOM element if you need to manually trigger the autosize.
 
 License
 ==
--- autosizeDirective.js
+++ autosizeDirective.js
@@ -1,8 +1,8 @@
 /*!
- * angular-textarea-autosize v1.2.1
- * (c) 2015 Ken Snyder
+ * angular-textarea-autosize v2.0.0
+ * (c) 2017 Ken Snyder
  * MIT License
- * 
+ *
  * Based on the following code and documentation:
  * https://github.com/jackmoore/autosize
  * https://github.com/javierjulio/textarea-autosize
@@ -10,54 +10,58 @@
  * https://developer.mozilla.org/en-US/docs/Web/API/Element.scrollHeight
  */
 (function(angular) {
-	
-	angular.module('textareaAutosize', [])
-	.directive('autosize', ['$timeout', function autosizeDirective($timeout) {
-		/*
-		Usage:
-		<!-- min rows of 1 -->
-		<textarea ng-model="note" autosize></textarea>
-		<!-- min rows of 2 -->
-		<textarea ng-model="note" autosize rows="2"></textarea>
-		<!-- min rows of 1, callback when size changes -->
-		<textarea ng-model="note" autosize="myOnResizeHandler"></textarea>
-		*/
-		return {
-			restrict: 'A',
-			scope: {
-				onResize: '&?autosize'
-			},
-			require: '?ngModel',
-			link: function autosizeDirectiveLink($scope, $textarea, attrs, ngModel) {
-				var sizer = setupAutosizer($textarea[0], $scope.onResize);
-				
-				if (ngModel) {
-					ngModel.$formatters.push(function(value) {
-						sizer.adjust();
-						return value;
-					});
-				}
-				$timeout(function() {
-					sizer.measure();
-					sizer.adjust();
-				});
-				// The autosizer will not respond to changes in the rows attribute 
-				// or the computed css values for border, padding, box-sizing or line-height
-				// so we add sort of hack to reinit manually if needed.
-				// Those changes could be auto detected with a MutationObserver 
-				// but that is outside the scope of this project right now.
-				$textarea[0].reinitAutosizer = function() {
-					sizer.measure();
-					sizer.adjust();
-				};
-			}
-		};
-	}]);
 
-	function setupAutosizer(textarea, onResize) {
-	  
+	angular.module('textareaAutosize', [])
+		.directive('autosize', function autosizeDirective() {
+			/*
+			 Usage:
+			 <!-- min rows of 1 -->
+			 <textarea ng-model="note" autosize></textarea>
+			 <!-- min rows of 2 -->
+			 <textarea ng-model="note" autosize rows="2"></textarea>
+			 */
+			return {
+				restrict: 'A',
+				require: 'ngModel',
+				link: function autosizeDirectiveLink($scope, $textarea, attrs, ngModel) {
+					var sizer = setupAutosizer($textarea[0]);
+
+					var run = function($0) {
+						if (!sizer.minHeight) {
+							sizer.measure();
+						}
+						sizer.adjust();
+						return $0;
+					};
+
+					// respond to final changes from user input
+					ngModel.$viewChangeListeners.push(run);
+					// respond to final changes from model
+					$scope.$watch(attrs.ngModel, run);
+					// respond to changes in padding or border
+					if ($textarea[0].style.transition === '') {
+						$textarea[0].style.transition = [
+							'border-top-width 1ms',
+							'border-bottom-width 1ms',
+							'padding-top 1ms',
+							'padding-bottom 1ms',
+							'line-height 1ms'
+						].join(', ');
+					}
+					$textarea.on('transitionend', function() {
+						sizer.measure();
+						sizer.adjust();
+					});
+					// provide a way to force resize programmatically
+					$textarea[0].reinitAutosize = run;
+				}
+			};
+		});
+
+	function setupAutosizer(textarea) {
+
 		var minHeight, lineHeight, extraHeight;
-	  
+
 		function _throttle(ms, fn) {
 			var inprogress = false;
 			return function() {
@@ -70,14 +74,11 @@
 				}, ms);
 			};
 		}
-		
+
 		function _getBoxSizing(style) {
-			return style.getPropertyValue('box-sizing') ||
-				style.getPropertyValue('-webkit-box-sizing') ||
-				style.getPropertyValue('-moz-box-sizing') ||
-				'content-box';
+			return style.getPropertyValue('box-sizing') || 'content-box';
 		}
-		
+
 		function _getLineHeight(style) {
 			lineHeight = style.getPropertyValue('line-height');
 			// line height will be returned in px or with keyword "normal" which is about 1.14 * font-size
@@ -89,7 +90,7 @@
 			}
 			return lineHeight;
 		}
-		
+
 		function _getBorderHeight(style) {
 			// border thickness can be a number value or one of thin, medium or thick
 			// regardless of the keyword or units, px values are returned by all browsers
@@ -97,20 +98,20 @@
 			return parseFloat(style.getPropertyValue('border-top-width') || 0) || 0 +
 				parseFloat(style.getPropertyValue('border-bottom-width') || 0) || 0;
 		}
-		
+
 		function _getPaddingHeight(style) {
 			return parseFloat(style.getPropertyValue('padding-top') || 0) || 0 +
 				parseFloat(style.getPropertyValue('padding-bottom') || 0) || 0;
 		}
-		
+
 		function setOverflow() {
-			// just in case we are splitting pixels, 
+			// just in case we are splitting pixels,
 			// we would rather see descending letters get cut off
 			// than have the scrollbar display and mess up our calculations
 			textarea.style.overflow = 'hidden';
 			textarea.style.resize = 'none';
 		}
-		
+
 		function measure() {
 			// get effective property values for height
 			var style = window.getComputedStyle(textarea, null);
@@ -128,26 +129,8 @@
 				(parseFloat(textarea.getAttribute('rows')) || 1) * lineHeight + extraHeight
 			);
 		}
-		
-		function observe() {
-			textarea.addEventListener('input', adjust, false);
-			if ('onpropertychange' in textarea) {
-				// Detects IE9. IE9 does not fire oninput for deletions,
-				// so binding to onkeyup to catch most of those occasions.
-				textarea.addEventListener('keyup', adjust, false);
-			}
-		}
-		
-		function unobserve() {
-			textarea.removeEventListener('input', adjust);
-			textarea.removeEventListener('keyup', adjust);
-		}
-		
+
 		function _adjuster() {
-			// if we have an onresize callback, we need to note the "before" height
-			if (onResize) {
-				var oldHeight = textarea.style.height;
-			}
 			var currentWindowScroll = window.scrollY;
 			// ensure that content can't fit so scrollHeight will be correct
 			textarea.style.height = '0';
@@ -161,22 +144,14 @@
 			if (currentWindowScroll != window.scrollY) {
 				window.scroll(window.scrollX, currentWindowScroll);
 			}
-			// trigger resize callback if height has changed
-			if (onResize && oldHeight != newHeight) {
-				onResize(parseFloat(oldHeight), newHeight);
-			}			
 		}
-		
+
 		var adjust = _throttle(0, _adjuster);
-		
+
 		setOverflow();
-		measure();
-		observe();
-		
+
 		return {
-			setOverflow: setOverflow,
-			observe: observe,
-			unobserve: unobserve,
+			minHeight: minHeight,
 			measure: measure,
 			adjust: adjust
 		};
