e7424e1ab8d84693f25332969bcf9039222fcaa4
Ken Snyder <kendsnyder@gmail.com>
2017-04-30 13:10:58 -0600

    Simplify autosize code and properly respond to model changes

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 Angular Textarea Autosize
 =
 
-v1.2.0 May 2015
+v1.2.1 April 2017
 
 Created because other libraries use ghost elements which didn't calculate very well for me, especially when using web fonts.
 
@@ -13,8 +13,8 @@ Usage
 <textarea ng-model="note" autosize></textarea>
 <!-- min rows of 2 -->
 <textarea ng-model="note" autosize rows="2"></textarea>
-<!-- min rows of 3, callback when size changes -->
-<textarea ng-model="note" autosize="{minRows:3, onresize:myHandler}"></textarea>
+<!-- min rows of 1, callback when size changes -->
+<textarea ng-model="note" autosize="myOnResizeHandler"></textarea>
 ```
 
 Edge cases:
@@ -22,7 +22,7 @@ Edge cases:
 1. If you have placeholder text that wraps to a new line, you'll need to manually set the rows attribute to fit the placeholder.
 2. The css property "resize" is forced to "none" so the textbox will have no resize handle
 3. If the textarea has a CSS transition that applies to the height, calculations will be off and size changes may appear janky
-4. If the textarea has a percent width and the window changes size, the autosizer will be triggered until the next keystroke
+4. If the textarea has a percent width and the window changes size, the autosizer will not be triggered until the next keystroke
 5. The autosizer will not respond to changes in the rows attribute or the computed css values for border, padding, box-sizing or line-height. A hack is to call .reinitAutosize() on the raw DOM element if you know any of those things happen.
 6. Only supports IE9+.
 
--- autosizeDirective.js
+++ autosizeDirective.js
@@ -1,5 +1,5 @@
 /*!
- * angular-textarea-autosize v1.2.0
+ * angular-textarea-autosize v1.2.1
  * (c) 2015 Ken Snyder
  * MIT License
  * 
@@ -12,36 +12,33 @@
 (function(angular) {
 	
 	angular.module('textareaAutosize', [])
-	.value('constructor', AutosizedTextarea)
-	.directive('autosize', ['$timeout','$window','constructor',
-  function autosizeDirective($timeout,  $window,  constructor) {
+	.directive('autosize', ['$timeout', function autosizeDirective($timeout) {
 		/*
 		Usage:
 		<!-- min rows of 1 -->
 		<textarea ng-model="note" autosize></textarea>
 		<!-- min rows of 2 -->
 		<textarea ng-model="note" autosize rows="2"></textarea>
-		<!-- min rows of 3, callback when size changes -->
-		<textarea ng-model="note" autosize="{minRows:3, onresize:myHandler}"></textarea>
+		<!-- min rows of 1, callback when size changes -->
+		<textarea ng-model="note" autosize="myOnResizeHandler"></textarea>
 		*/
 		return {
 			restrict: 'A',
 			scope: {
-				options: '=?autosize'
+				onResize: '&?autosize'
 			},
 			require: '?ngModel',
 			link: function autosizeDirectiveLink($scope, $textarea, attrs, ngModel) {
-				$scope.options = $scope.options || {};
-				var sizer = new constructor({
-					$scope: $scope,
-					$textarea: $textarea,
-					attrs: attrs,
-					$window: $window,
-					ngModel: ngModel
-				});
+				var sizer = setupAutosizer($textarea[0], $scope.onResize);
+				
+				if (ngModel) {
+					ngModel.$formatters.push(function(value) {
+						sizer.adjust();
+						return value;
+					});
+				}
 				$timeout(function() {
-					sizer.setup();
-					sizer.observe();
+					sizer.measure();
 					sizer.adjust();
 				});
 				// The autosizer will not respond to changes in the rows attribute 
@@ -50,126 +47,139 @@
 				// Those changes could be auto detected with a MutationObserver 
 				// but that is outside the scope of this project right now.
 				$textarea[0].reinitAutosizer = function() {
-					sizer.setup();
+					sizer.measure();
 					sizer.adjust();
 				};
 			}
 		};
 	}]);
 
-	function throttle(ms, fn) {
-		var inprogress = false;
-		return function() {
-			if (!inprogress) {
-				fn();
+	function setupAutosizer(textarea, onResize) {
+	  
+		var minHeight, lineHeight, extraHeight;
+	  
+		function _throttle(ms, fn) {
+			var inprogress = false;
+			return function() {
+				if (!inprogress) {
+					fn();
+				}
+				inprogress = true;
+				setTimeout(function() {
+					inprogress = false;
+				}, ms);
+			};
+		}
+		
+		function _getBoxSizing(style) {
+			return style.getPropertyValue('box-sizing') ||
+				style.getPropertyValue('-webkit-box-sizing') ||
+				style.getPropertyValue('-moz-box-sizing') ||
+				'content-box';
+		}
+		
+		function _getLineHeight(style) {
+			lineHeight = style.getPropertyValue('line-height');
+			// line height will be returned in px or with keyword "normal" which is about 1.14 * font-size
+			if (lineHeight == 'normal') {
+				lineHeight = (parseFloat(style.getPropertyValue('font-size')) || 16) * 1.14;
 			}
-			inprogress = true;
-			setTimeout(function() {
-				inprogress = false;
-			}, ms);
-		};
-	}	
-
-	function AutosizedTextarea() {
-		this.initialize.apply(this, [].slice.call(arguments));
-	}
-	
-	AutosizedTextarea.prototype = {
-		initialize: function(vars) {
-			this.$scope = vars.$scope;
-			this.$window = vars.$window;
-			this.$textarea = vars.$textarea;
-			this.ngModel = vars.ngModel;
-			this.attrs = vars.attrs;
-			this.textarea = this.$textarea[0];
-		},
-		setup: function() {
+			else {
+				lineHeight = parseFloat(lineHeight);
+			}
+			return lineHeight;
+		}
+		
+		function _getBorderHeight(style) {
+			// border thickness can be a number value or one of thin, medium or thick
+			// regardless of the keyword or units, px values are returned by all browsers
+			// http://codepen.io/kendsnyder/pen/vOKRwZ
+			return parseFloat(style.getPropertyValue('border-top-width') || 0) || 0 +
+				parseFloat(style.getPropertyValue('border-bottom-width') || 0) || 0;
+		}
+		
+		function _getPaddingHeight(style) {
+			return parseFloat(style.getPropertyValue('padding-top') || 0) || 0 +
+				parseFloat(style.getPropertyValue('padding-bottom') || 0) || 0;
+		}
+		
+		function setOverflow() {
 			// just in case we are splitting pixels, 
 			// we would rather see descending letters get cut off
 			// than have the scrollbar display and mess up our calculations
-			this.textarea.style.overflow = 'hidden';
-			this.textarea.style.resize = 'none';
+			textarea.style.overflow = 'hidden';
+			textarea.style.resize = 'none';
+		}
+		
+		function measure() {
 			// get effective property values for height
-			var style = this.$window.getComputedStyle(this.textarea, null);
+			var style = window.getComputedStyle(textarea, null);
 			// note that css values can be fractional
-			this.lineHeight = style.getPropertyValue('line-height');
-			// line height will be returned in px or with keyword "normal" which is about 1.14 * font-size
-			if (this.lineHeight == 'normal') {
-				this.lineHeight = (parseFloat(style.getPropertyValue('font-size')) || 16) * 1.14;
+			lineHeight = _getLineHeight(style);
+			// calculate the final extra height based on box sizing
+			extraHeight = 0;
+			switch (_getBoxSizing(style)) {
+				// border-box includes border height and padding height
+				case 'border-box': extraHeight += _getBorderHeight(style);
+				// padding-box includes only padding height
+				case 'padding-box': extraHeight += _getPaddingHeight(style);
 			}
-			else {
-				this.lineHeight = parseFloat(this.lineHeight);
-			}
-			this.paddingHeight = 
-				parseFloat(style.getPropertyValue('padding-top') || 0) || 0
-				+ parseFloat(style.getPropertyValue('padding-bottom') || 0) || 0
-			;
-			// border thickness can be a number value or one of "thin, medium thick."
-			// regardless of the keyword or units, px values are returned by all browsers
-			// http://codepen.io/kendsnyder/pen/vOKRwZ
-			this.borderHeight = 
-				parseFloat(style.getPropertyValue('border-top-width') || 0) || 0
-				+ parseFloat(style.getPropertyValue('border-bottom-width') || 0) || 0
-			;
-			this.boxSizing = (
-				style.getPropertyValue('box-sizing') ||
-				style.getPropertyValue('-webkit-box-sizing') ||
-				style.getPropertyValue('-moz-box-sizing') ||
-				'content-box'
+			minHeight = Math.ceil(
+				(parseFloat(textarea.getAttribute('rows')) || 1) * lineHeight + extraHeight
 			);
-			this.extraHeight = 0;
-			if (this.boxSizing == 'border-box' || this.boxSizing == 'padding-box') {
-				this.extraHeight += this.paddingHeight;
-			}
-			if (this.boxSizing == 'border-box') {
-				this.extraHeight += this.borderHeight;
-			}
-			this.minHeight = Math.ceil(
-				(this.$scope.options.minRows || parseFloat(this.attrs.rows) || 1) 
-				* this.lineHeight 
-				+ this.extraHeight
-			);
-		},
-		observe: function() {
-			var events = 'input';
-			if ('onpropertychange' in this.textarea) {
+		}
+		
+		function observe() {
+			textarea.addEventListener('input', adjust, false);
+			if ('onpropertychange' in textarea) {
 				// Detects IE9. IE9 does not fire oninput for deletions,
 				// so binding to onkeyup to catch most of those occasions.
-				events += ' keyup';
-			}
-			// Listen for both keyboard events and view changes
-			// but use throttle to avoid calling both in the same event loop.
-			var self = this;
-			var adjust = throttle(0, function() {
-				self.adjust();
-			});
-			this.$textarea.on(events, adjust);
-			if (this.ngModel) {
-				this.ngModel.$viewChangeListeners.push(adjust);
-			}
-		},
-		adjust: function() {
-			// if we have an onresize callback, we need to note the "before" height
-			if (this.$scope.options.onresize) {
-				var oldHeight = this.textarea.style.height;
-			}
-			var currentWindowScroll = this.$window.scrollY;
-			// ensure that content can't fit so scrollHeight will be correct
-			this.textarea.style.height = '0';
-			// set height that is just tall enough
-			// note that scrollHeight is always an integer
-			var newHeight = Math.max(this.minHeight, this.textarea.scrollHeight);
-			this.textarea.style.height = newHeight + 'px';
-			// put the window scroll position back
-			// since setting height to 0 may cause window scroll to change
-			if (currentWindowScroll != this.$window.scrollY) {
-				this.$window.scroll(this.$window.scrollX, currentWindowScroll);
-			}
-			// trigger resize callback if height has changed
-			if (this.$scope.options.onresize && oldHeight != newHeight) {
-				this.$scope.options.onresize.call( this, parseFloat(oldHeight), newHeight );
+				textarea.addEventListener('keyup', adjust, false);
 			}
 		}
-	};
+		
+		function unobserve() {
+			textarea.removeEventListener('input', adjust);
+			textarea.removeEventListener('keyup', adjust);
+		}
+		
+		function _adjuster() {
+			// if we have an onresize callback, we need to note the "before" height
+			if (onResize) {
+				var oldHeight = textarea.style.height;
+			}
+			var currentWindowScroll = window.scrollY;
+			// ensure that content can't fit so scrollHeight will be correct
+			textarea.style.height = '0';
+			// set height that is just tall enough
+			// note that scrollHeight is always an integer
+			// Adding 1 seems to fix some cases where textarea jumps to scrollTop 1
+			var newHeight = Math.max(minHeight, textarea.scrollHeight) + 1;
+			textarea.style.height = newHeight + 'px';
+			// put the window scroll position back
+			// since setting height to 0 may cause window scroll to change
+			if (currentWindowScroll != window.scrollY) {
+				window.scroll(window.scrollX, currentWindowScroll);
+			}
+			// trigger resize callback if height has changed
+			if (onResize && oldHeight != newHeight) {
+				onResize(parseFloat(oldHeight), newHeight);
+			}			
+		}
+		
+		var adjust = _throttle(0, _adjuster);
+		
+		setOverflow();
+		measure();
+		observe();
+		
+		return {
+			setOverflow: setOverflow,
+			observe: observe,
+			unobserve: unobserve,
+			measure: measure,
+			adjust: adjust
+		};
+	}
 
 })(angular);
